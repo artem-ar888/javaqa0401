6eeae632d447ca97097e8d107bc08bd88e7c4144
Artem Arafailov <artem.ar888@gmail.com>
2024-11-18 18:24:18 +0300

    Variables have been renamed

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,28 @@
 public class Main {
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
-        int price = 10_000;
-        int miles = service.calculate(price); // должно получиться 500
+        int ticketPrice = 10_000;
+        int bonusMiles = service.calculate(ticketPrice); // должно получиться 500
         System.out.println();
-        System.out.println("За билет стоимостью " + price + " руб.");
-        System.out.println("Начислено бонусных милей: " + miles);
+        System.out.println("За билет стоимостью " + ticketPrice + " руб.");
+        System.out.println("Начислено бонусных милей: " + bonusMiles);
 
-        price = 1_000;
-        miles = service.calculate(price);
+        ticketPrice = 1_000;
+        bonusMiles = service.calculate(ticketPrice);
         System.out.println();
-        System.out.println("За билет стоимостью " + price + " руб.");
-        System.out.println("Начислено бонусных милей: " + miles);
+        System.out.println("За билет стоимостью " + ticketPrice + " руб.");
+        System.out.println("Начислено бонусных милей: " + bonusMiles);
 
-        price = 50;
-        miles = service.calculate(price);
+        ticketPrice = 50;
+        bonusMiles = service.calculate(ticketPrice);
         System.out.println();
-        System.out.println("За билет стоимостью " + price + " руб.");
-        System.out.println("Начислено бонусных милей: " + miles);
+        System.out.println("За билет стоимостью " + ticketPrice + " руб.");
+        System.out.println("Начислено бонусных милей: " + bonusMiles);
 
-        price = 19;
-        miles = service.calculate(price);
+        ticketPrice = 19;
+        bonusMiles = service.calculate(ticketPrice);
         System.out.println();
-        System.out.println("За билет стоимостью " + price + " руб.");
-        System.out.println("Начислено бонусных милей: " + miles);
+        System.out.println("За билет стоимостью " + ticketPrice + " руб.");
+        System.out.println("Начислено бонусных милей: " + bonusMiles);
     }
 }
